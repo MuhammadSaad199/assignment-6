36f3eaf12f18df2e4345bd4fe5ebeed03a457d1c
M.Saad <mohammadsaadjavediqbal1999@gmail.com>
2020-06-15 20:18:16 +0500

    JS asssignment

---
+++ app.js
@@ -0,0 +1,401 @@
+1// alert("AOA");
+
+
+2// alert("Error! Please enter a valid password.");
+
+
+// 3 alert("Welcome to js Land..." +"<br>"   "Happy Coding");
+
+
+4// alert("Welcome JS Land");
+// alert("Happy Coding!");
+
+
+5// alert("");
+
+
+
+    //   2 chapter
+
+
+1// var username = "Ali";
+//  alert(username);
+
+
+2//  var myname = "M.Saad";
+//  alert(myname);
+
+
+3// var message = "Hello World";
+// alert(message);
+
+
+4// var name = "Jhone Doe";
+// alert(name);
+// var age = "15 Years Old";
+// alert(age);
+// var course = "Certified Mobile Application Development";
+// alert(course);
+
+
+5// var eat = "PIZZA" ;
+// alert(eat);
+
+
+6// var email = "My email address is example@example.com";
+// alert(email);
+
+
+7// var book = "I am trying to learn from the Book A smarter way to learn JavaScript";
+// alert(book);
+
+
+8// var show = "Yah! I can write HTML content through JavaScript"
+// document.write(show);
+
+
+9// var 
+// alert
+
+
+
+
+//    3 chapter
+
+
+
+1// var age = "I am 15 Years Old";
+// alert(age);
+
+
+2// var site = "You have visited this site 12 times.";
+// alert(site);
+
+
+
+3//  var birthyear = "My birth year is 2020";
+//  document.write(birthyear);
+
+
+
+4//  var store = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store.";
+//  document.write(store);
+
+
+
+
+    // 4 chapter
+
+
+
+1// var name = "My Name is Asad";
+// var age = "And I Am 5 Years Old";
+// var class = "I Read In KG_1";
+
+
+2//   legal variable
+//    1) letters 
+//    2) alpha sign
+//    3) dollar sign 
+//    4) underscores 
+//    5)  
+
+
+
+2//   illegal variable
+//    1) numerics 
+//    2) (?)question mark 
+//    3) % 
+//    4) !
+//    5)  *
+
+
+
+3// var rules = "Rules for naming JS variable <br><br><br><br>";
+// document.write(rules);
+
+// var store = "Variable names can only contain letters, dollar sign, alpha sign and underscores <br>";
+// document.write(store);
+
+// var store = "Variable must begin with a _first, $second or third <br>";
+// document.write(store);
+
+// var store = "Variable names are case sensitive <br>";
+// document.write(store);
+
+// var store = "Variable names should not be JS keywords <br>";
+// document.write(store);
+
+
+
+
+//   5 chapter
+
+
+
+
+1// var a = 3;
+// var b = 5;
+// var c = a+b;
+// document.write(c);
+
+
+2// var a = 3;
+// var b = 5;
+// var c = a-b;
+// document.write(c);
+
+2// var a = 3;
+// var b = 5;
+// var c = a*b;
+// document.write(c);
+
+2// var a = 3;
+// var b = 5;
+// var c = a/b;
+// document.write(c);
+
+2//  var a = 3;
+//  var b = 5;
+//  var c = a%b;
+//  document.write(c);
+
+
+3// var show = 5
+// document.write("Initial value: " + show + "<br>" );
+// var a = ++show;
+// document.write("Value after increment is: " + a + "<br>" );
+// var b = a+7;
+// document.write("Value after addition is: " + b + "<br>" );
+// var c = --b;
+// document.write("Value after decrement is: " + c + "<br>" );
+// var d = c/3;
+// var e = 0/d;
+// document.write("The remainder is : " + e);
+
+
+
+4// var a = 600;
+// var b = 5;
+// var c = a*b;
+// document.write("Total coast to buy 5 tickets to a movie is " + c + "PKR");
+
+
+5// for(var a = 1; a <= 10; a++){
+//     document.write("4" + "×" + a + "=" + 4*a + "<br>");
+// }
+
+
+6// var celsius = 32;
+// var convert1 = (celsius*9/5)+32;
+// var fahrenheit = convert1;
+// var convert2 = (fahrenheit-32)*5/9
+// document.write(convert2 + "C is"+ " " + convert1 +"F" + "<br>");
+// document.write(convert1 + "F is " + convert2 + "C");
+
+
+7//  var item1 = 400;
+// var item2 = 530;
+// var quantity1 = 2;
+// var quantity2 = 4;
+// var shipping = 100;
+// var total = item1*quantity1 + item2*quantity2 + shipping
+// document.write("Total cost of your order is " + total);
+
+
+8// var total = 850;
+// var obtained = 519;
+// var percentage = obtained/total*100;
+// document.write("Percentage: " + percentage + "%");
+
+
+9// var dollar = 104.80;
+// var riyal = 28;
+// var  rupee = dollar*10 + riyal*25 ;
+// document.write("Total Currency in PKR: " + rupee)
+
+
+10// var i = 7;
+// document.write(i+5 + "<br>");
+// document.write(i*10 + "<br>");
+// document.write(i/2  + "<br>");
+
+
+11// var currentyear = 2020;
+// var birthyear = 1999;
+// var yourage = currentyear-birthyear
+// document.write("Current Year: " + currentyear +"<br>");
+// document.write("Birth Year: " + birthyear +"<br>");
+// document.write("Your Age is: " + yourage +"<br>");
+
+
+12// var radius = 15;
+// var circumference = 2*3.142*15;
+// document.write("Radius of a circle: " + radius + "<br>");
+// document.write("The circumference is: " + circumference + "<br>");
+// document.write("The area is: " + 3.142*15*2);
+
+
+13// var snack = "lays";
+// var current = 20;
+// var max = 99;
+// var perday = 3;
+// var restlife = 3*365*(max-current)
+// document.write("Favourity Snack: " + snack +"<br>");
+// document.write("Current age: " + current +"<br>");
+// document.write("Estimated Maximum Age: " + max +"<br>");
+// document.write("Amount oh snack per day: " + perday +"<br>");
+// document.write("You will need " + restlife + " " + snack + " to last you until the ripe old age of 99");
+
+
+
+
+//    6-9 chapter
+
+
+1// var z = 10;
+//  var result = "Result";
+// document.write(result + ":" + "<br>");
+// document.write("The value of a is: " + z + "<br>");
+// var x = "..............................."
+// document.write(x + "<br>"  + "<br>");
+// document.write("The value of ++a is: " + ++z + "<br>");
+// document.write("Now the value of a is: " + z + "<br>" + "<br>");
+// document.write("The value of a++ is: " + z + "<br>" );
+// document.write("Now the value of a is: " + ++z + "<br>" + "<br>");
+// document.write("The value of --a is: " + --z + "<br>" );
+// document.write("Now the value of a is: " + z + "<br>" + "<br>");
+// document.write("The value of a-- is: " + z + "<br>" );
+// document.write("Now the value of a is: " + --z);
+
+
+2// var a = 5, b =2;
+// document.write("a is " + 5 +"<br>");
+// document.write("b is " + 2 +"<br>");
+// document.write("result is " + (--a - --b + ++b + b--));
+
+
+3// var greet = "thanks";
+// var name = "Basit";
+// alert(greet + " " + name + " :)");
+
+
+
+// document.write("<table>");
+// document.write("Subject ");
+
+// document.write("Total Marks");
+
+// document.write(" Obtained Marks ");
+
+// document.write("Percentage");
+// document.write("</table>");
+
+
+// document.write("<table>");
+// var a = "Math";
+// document.write(a);
+// document.write("</table>");
+
+// document.write("<table>");
+// var b = "Physics"; 
+// document.write(b);
+// document.write("</table>");
+
+// document.write("<table>");
+// var c = "Chemistry";
+// document.write(c);
+// document.write("</table>");
+
+
+
+// var x = 100;  
+// document.write(x);
+
+// var  x = 100; 
+// document.write(x);
+
+
+// var x = 100;
+// document.write(x);
+
+
+
+// document.write("<table>");
+// document.write(z + z + z);
+// document.write("</table>");
+
+
+
+// document.write("<table>");
+// var first = 66 , second = 83 , third = 53;
+// document.write(first);
+// var no = first + second + third;
+// document.write("</table>");
+
+
+// var total = no/300*100;
+// document.write("</table>");
+
+
+
+
+
+
+
+//   17 to 20 chapter
+
+
+3//  for(var a = 1; a <= 10; ++a ){
+//      document.write(a  + "<br>");
+//  }
+
+
+4// var a = prompt("Enter a number to show its multiplication table");
+// var b = prompt("Enter length multiplication table");
+
+// for(var b = 1; b <= 20; ++b){
+//     document.write(a + " x" + b + "=" + a*b +"<br>");
+// }
+
+
+
+10// var a = 5;
+// for(var b = 1; b <= 20; ++b){
+//     document.write(a*b +", ");
+// }
+
+
+// var b = prompt("Welcome to Ashrafi Bakery. What do you want to order sir/ma'am?");
+
+// var sweet = ["cake" , "apple pie" , "cookie" , "chips" , "patties"];
+
+// for(var i = 0; i <= sweet.length; ++i){
+//     if(sweet[i] === "patties"){
+//         alert(sweet[i] + " is not available")
+//         break
+//     }
+
+   
+//       }
+
+
+
+
+
+document.write("counting:" +"<br>"+"<br>");
+ for(var a = 1; a <= 15; ++a ){
+    document.write(a + ", " );
+  }
+
+  document.write("<br>" +"<br>" + "Reverse:" +"<br>"+"<br>");
+  for(var a = 15; a > 0; --a ){
+     document.write(a +1 + ", ");
+   }
+ 
+
+   document.write("<br>" +"<br>" + "Even:" +"<br>"+"<br>");
+   for(var a = 1; a <= 15; a++ ){
+      document.write(1+a + ", ");
+    }
+
